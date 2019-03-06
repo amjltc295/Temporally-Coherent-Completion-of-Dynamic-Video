0ab1ba6db91060e53080aaa10132c960aad21964
Ya-Liang Chang (Allen) <b03901014@ntu.edu.tw>
2019-03-06 20:21:00 +0800

    Update run_all.m

--- run_all.m
+++ run_all.m
@@ -1,11 +1,20 @@
+fprintf('Start Time:')
+start_time = clock
 % Get a list of all files and folders in this folder.
-files = dir('./dataset/test_20181109_videos');
+files = dir('./dataset/video');
+% files = dir('./dataset/test_20181109_videos');
 % Get a logical vector that tells which is a directory.
 dirFlags = [files.isdir];
 video_names = files(~dirFlags);
 
-for i = 12 : length(video_names)
+for i = 1 : length(video_names)
+    video_time = clock
     video_name = video_names(i).name;
     video_name = split(video_name, '.');
     vc_complete(video_name{1})
+    fprintf('Video Time:')
+    clock - video_time
 end
+
+fprintf('Total Time:')
+clock - start_time
