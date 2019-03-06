6267886cbce07db4d0f8020e32a34c5f8b995665
Ya-Liang Chang (Allen) <b03901014@ntu.edu.tw>
2019-03-06 21:19:26 +0800

    Add try/catch for run_all

--- run_all.m
+++ run_all.m
@@ -6,15 +6,24 @@ files = dir('./dataset/video');
 % Get a logical vector that tells which is a directory.
 dirFlags = [files.isdir];
 video_names = files(~dirFlags);
+error_num = 0;
 
 for i = 1 : length(video_names)
-    video_time = clock
-    video_name = video_names(i).name;
-    video_name = split(video_name, '.');
-    vc_complete(video_name{1})
-    fprintf('Video Time:')
-    clock - video_time
+    try
+        video_time = clock
+        video_name = video_names(i).name;
+        video_name = split(video_name, '.');
+        vc_complete(video_name{1})
+        fprintf('Video Time:')
+        clock - video_time
+    catch e %e is an MException struct
+        fprintf(1,'The identifier was:\n%s',e.identifier);
+        fprintf(1,'There was an error! The message was:\n%s',e.message);
+        error_num = error_num + 1;
+    end
 end
 
 fprintf('Total Time:')
 clock - start_time
+fprintf('Error num:')
+error_num;
